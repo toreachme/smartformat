e4e9a57fd71624137dcf9fa99d7a2f8bb287f2df
Emmanuel <emonuoha@gmail.com>
2018-05-04 17:31:29 -0400

    friday may4 commit

--- application/controllers/Gravity.php
+++ application/controllers/Gravity.php
@@ -1,5 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+require_once "./phpword/vendor/autoload.php";
 
 class Gravity extends CI_Controller {
 
@@ -29,28 +30,30 @@ class Gravity extends CI_Controller {
 		$formsFieldsArray = $forms['response']['fields'];
 		$formEntriesArray = $formEntries['response']['entries'];
 
+		$this->formatData($formsFieldsArray, $formEntriesArray);
 
-		foreach ($formsFieldsArray as $formsfields) {
-			//Check for only Section Headers
-			if ($formsfields['type'] == 'section') {
-				echo "<h3>".$formsfields['label']."</h3><br/>";
-			}
-			// Add form field ID to a variable for ease of use
-			$fieldId = $formsfields['id'];
 
-			//Loop through Form Entries
-			foreach ($formEntriesArray as $entries) {
-				foreach ($entries as $key => $value) {
-					//Make sure only Questions show up not (Sections and HTML types) 
-					if ($key == $fieldId && ($formsfields['type'] != 'section' && $formsfields['type'] != 'html')) {
-						//Output Questions
-						echo "<b>".$formsfields['label']."</b><br/>";
-						//Output Answers
-						echo $value."<br/><br/>";
-					}
-				}
-			}
-		}
+		// foreach ($formsFieldsArray as $formsfields) {
+		// 	//Check for only Section Headers
+		// 	if ($formsfields['type'] == 'section') {
+		// 		echo "<h3>".$formsfields['label']."</h3><br/>";
+		// 	}
+		// 	// Add form field ID to a variable for ease of use
+		// 	$fieldId = $formsfields['id'];
+
+		// 	//Loop through Form Entries
+		// 	foreach ($formEntriesArray as $entries) {
+		// 		foreach ($entries as $key => $value) {
+		// 			//Make sure only Questions show up not (Sections and HTML types) 
+		// 			if ($key == $fieldId && ($formsfields['type'] != 'section' && $formsfields['type'] != 'html')) {
+		// 				//Output Questions
+		// 				echo "<b>".$formsfields['label']."</b><br/>";
+		// 				//Output Answers
+		// 				echo $value."<br/><br/>";
+		// 			}
+		// 		}
+		// 	}
+		// }
 
 
 	}	
@@ -93,7 +96,6 @@ class Gravity extends CI_Controller {
 
 	/*
 	 * Helper methods
-	 *
 	 */
 
 	private function signString($route){
@@ -124,6 +126,77 @@ class Gravity extends CI_Controller {
 	private function getUrlContents($url){
 		$fileOutput = file_get_contents($url);
 		return $fileOutput;
+	}
+
+
+
+	/*
+	* Library 
+	*/
+
+	public function formatData($formsFieldsArray, $formEntriesArray){
+		// Creating the new document...
+		$phpWord = new \PhpOffice\PhpWord\PhpWord();
+
+
+		// Add title styles
+		$phpWord->addTitleStyle(1, array('size' => 10, 'name' => 'Arial','color'=>'333333', 'bold'=>true, 'underline' => \PhpOffice\PhpWord\Style\Font::UNDERLINE_SINGLE),array('justify' => \PhpOffice\PhpWord\SimpleType\Jc::JUSTIFY));
+
+        //Question styles
+		$phpWord->addFontStyle('question_font', array('size' => 10, 'name' => 'Arial','bold'=>true, 'color'=>'4F81BD'));
+		$phpWord->addParagraphStyle('question_paragraph', array('justify' => \PhpOffice\PhpWord\SimpleType\Jc::JUSTIFY));
+		$predefinedMultilevelStyle = array('listType' => \PhpOffice\PhpWord\Style\ListItem::TYPE_NUMBER);
+
+		$phpWord->addFontStyle('answers_font', array('size' => 10, 'name' => 'Arial'));
+		$phpWord->addParagraphStyle('answers_paragraph', array('indent' => 1,'justify' => \PhpOffice\PhpWord\SimpleType\Jc::JUSTIFY));
+
+
+
+		$section = $phpWord->addSection();
+
+
+
+			foreach ($formsFieldsArray as $formsfields) {
+				//Check for only Section Headers
+				if ($formsfields['type'] == 'section') {
+					$section->addTitle(htmlspecialchars($formsfields['label']), 1);
+					$section->addTextBreak();
+				}
+
+			    // Add form field ID to a variable for ease of use
+				$fieldId = $formsfields['id'];
+
+
+			    //Loop through Form Entries
+				foreach ($formEntriesArray as $entries) {
+					foreach ($entries as $key => $value) {
+
+					//Make sure only Questions show up not (Sections and HTML types) 
+						if ($key == $fieldId && ($formsfields['type'] != 'section' && $formsfields['type'] != 'html')) {
+
+						//Output Questions
+							$section->addListItem(htmlspecialchars($formsfields['label']), 0, 'question_font', $predefinedMultilevelStyle, 'question_paragraph');
+
+						//Output Answers
+							$section->addText(htmlspecialchars($value), 'answers_font', 'answers_paragraph');
+							$section->addTextBreak();
+
+						}
+					}
+				}
+
+				$section->addTextBreak();
+				// $section->addTextBreak();
+
+			}
+
+
+		$phpWord->getCompatibility()->setOoxmlVersion(15);
+
+		// Saving the document as OOXML file...
+		$objWriter = \PhpOffice\PhpWord\IOFactory::createWriter($phpWord, 'Word2007');
+		$objWriter->save('texttitleword.docx');
+
 	}
 
 }
